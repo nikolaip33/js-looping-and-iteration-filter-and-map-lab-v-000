50cb77104d814c38bc59c9a66f3db543c3f9de9c
Nikolai Pawlowski <nikolaip@gmail.com>
2019-02-06 21:02:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function driverNamesWithRevenueOver(drivers, revenue) {
 
 function exactMatch(drivers, attr) {
   key = Object.keys(attr)[0];
-  return drivers.filter(function(driver) {
+  return drivers.map(function(driver) {
 
     console.log(`${driver[key]} and ${attr[key]}`);
     driver[key] === attr[key];
