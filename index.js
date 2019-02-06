4afb0eb29e90e6a295590d1fd8e592ece03ea584
Nikolai Pawlowski <nikolaip@gmail.com>
2019-02-06 20:33:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function driverNamesWithRevenueOver(drivers, revenue) {
 function exactMatch(drivers, attr) {
   console.log(Object.keys(attr))
   return drivers.filter(function(driver) {
-    
+
   });
 }
